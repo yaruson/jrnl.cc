e99b7d5252f87e1bb159f3db2c9be51ac583bb7c
Nikolai Kim <n.kim@admitad.pro>
2021-06-18 15:32:22 +0300

    fix: remove favicon entirely

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,6 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/water.css@2/out/water.css' }
     ]
   },
